48789bfd0a89a45de44eaae671d92ad88b2aba41
Hirusha Sipsara <hirushasipsara@gmail.com>
2025-09-20 21:42:19 +0530

    Login Debug

--- Backend/src/main/java/com/ooadproject/backend/dto/RegisterRequestDTO.java
+++ Backend/src/main/java/com/ooadproject/backend/dto/RegisterRequestDTO.java
@@ -1,5 +1,6 @@
 package com.ooadproject.backend.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -7,6 +8,7 @@ import lombok.Data;
 
 @Data
 public class RegisterRequestDTO {
+    @JsonAlias({"name", "username"})
     @NotBlank(message = "Username is required")
     @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
     private String username;
