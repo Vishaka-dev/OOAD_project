0a0555e39dab2d99a8efd3821d72e7ab2e3ad849
Hirusha Sipsara <hirushasipsara@gmail.com>
2025-09-22 22:20:25 +0530

    Fix case sensitivity: rename Backend to backend

--- Backend/src/main/java/com/ooadproject/backend/repositories/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ooadproject.backend.repositories;
-
-import com.ooadproject.backend.entities.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-}
